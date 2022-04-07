a8ac96d319eb81158046b8c0648e5dafcdd77991
VanillaNk2006 <77216086+VanillaNk2006@users.noreply.github.com>
2022-04-07 10:08:51 -0500

    make it better

--- Sources/Models/Menus/divs.js
+++ Sources/Models/Menus/divs.js
@@ -6,6 +6,6 @@ module.exports = function misc(prefix) {
 return `\`\`\`
 ${prefix}8ball    ${prefix}alert    ${prefix}ascii  ${prefix}cucumber
 ${prefix}rainbow  ${prefix}roll     ${prefix}rps    ${prefix}clyde
-${prefix}wasted   ${prefix}confession
+${prefix}ship     ${prefix}wasted   ${prefix}confession
 \`\`\``
 }
